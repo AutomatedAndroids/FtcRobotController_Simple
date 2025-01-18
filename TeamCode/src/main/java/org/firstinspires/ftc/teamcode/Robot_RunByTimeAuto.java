1191bb0959be44d84cf1796c4b6fcffaf5f609ce
njnovo <75146085+njnovo@users.noreply.github.com>
2025-01-17 21:27:34 -0700

    The new auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_RunByTimeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_RunByTimeAuto.java
@@ -32,7 +32,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
@@ -54,31 +58,42 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
-public class RobotAutoDriveByTime_Linear extends LinearOpMode {
+@Autonomous(name="Score By Time", group="Robot")
+public class Robot_RunByTimeAuto extends LinearOpMode {
 
     /* Declare OpMode members. */
-    private DcMotor         leftDrive   = null;
-    private DcMotor         rightDrive  = null;
-
+    private DcMotor leftDrive, rightDrive, armMotor, leftActuator, rightActuator;
+    public CRServo intake = null; //the active intake servo
+    public Servo wrist = null; //the wrist servo
     private ElapsedTime     runtime = new ElapsedTime();
-
-
-    static final double     FORWARD_SPEED = 0.9;
+    final double ARM_TICKS_PER_DEGREE = 19.7924893140647;
+    final double ARM_ATTACH_HANGING_HOOK = 133   * ARM_TICKS_PER_DEGREE;
+    static final double     FORWARD_SPEED = 0.5;
+    static final double     ARM_FORWARD_SPEED = 0.8;
     static final double     TURN_SPEED    = 0.5;
-
+    final double WRIST_FOLDED_IN = 0.8333;
+    final double WRIST_FOLDED_OUT = 0.5;
     @Override
     public void runOpMode() {
 
         // Initialize the drive system variables.
-        leftDrive  = hardwareMap.get(DcMotor.class, "leftWheel");
-        rightDrive = hardwareMap.get(DcMotor.class, "rightWheel");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftWheel"); //the left
+        rightDrive = hardwareMap.get(DcMotor.class, "rightWheel"); //the right
+        armMotor = hardwareMap.get(DcMotor.class, "arm"); //the arm motor
 
+        leftActuator = hardwareMap.get(DcMotor.class, "leftAct");
+        rightActuator = hardwareMap.get(DcMotor.class, "rightAct");
+
+        intake = hardwareMap.get(CRServo.class, "frontServo");
+        wrist = hardwareMap.get(Servo.class, "turnServo");
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftActuator.setDirection(DcMotor.Direction.FORWARD);
+        rightActuator.setDirection(DcMotor.Direction.FORWARD);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
@@ -87,11 +102,11 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         // Wait for the game to start (driver presses START)
         waitForStart();
 
-        // Step through each leg of the path, ensuring that the OpMode has not been stopped along the way.
 
-        // Step 3:  Drive forward for 1 Second
-        leftDrive.setPower(-FORWARD_SPEED);
-        rightDrive.setPower(-FORWARD_SPEED);
+
+
+        leftDrive.setPower(FORWARD_SPEED);
+        rightDrive.setPower(FORWARD_SPEED);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 0.4)) {
             telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
@@ -102,8 +117,86 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         leftDrive.setPower(0);
         rightDrive.setPower(0);
 
+        //Put the arm in scoring settings
+        armMotor.setTargetPosition((int) (ARM_ATTACH_HANGING_HOOK));
+
+        ((DcMotorEx) armMotor).setVelocity(2100);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //Set the wrist position
+        wrist.setPosition(WRIST_FOLDED_IN);
+        sleep(1000);
+        //Put the sliders up
+        leftActuator.setPower(ARM_FORWARD_SPEED);
+        rightActuator.setPower(ARM_FORWARD_SPEED);
+
         telemetry.addData("Path", "Complete");
         telemetry.update();
-        sleep(1000);
+
+
+
+        sleep(1900);
+
+
+        leftActuator.setPower(0);
+        rightActuator.setPower(0);
+
+        armMotor.setTargetPosition((int) (ARM_ATTACH_HANGING_HOOK));
+        ((DcMotorEx) armMotor).setVelocity(2100);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        sleep(5000);
+
+        armMotor.setTargetPosition((int) (ARM_ATTACH_HANGING_HOOK));
+        ((DcMotorEx) armMotor).setVelocity(2100);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wrist.setPosition(WRIST_FOLDED_OUT);
+        leftDrive.setPower(0.3);
+        rightDrive.setPower(0.3);
+
+
+        sleep(500);
+        leftDrive.setPower(0.5);
+        rightDrive.setPower(-0.5);
+        sleep(900);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        wrist.setPosition(WRIST_FOLDED_OUT);
+        armMotor.setTargetPosition((int) (ARM_ATTACH_HANGING_HOOK));
+        ((DcMotorEx) armMotor).setVelocity(2100);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        intake.setPower(0.7);
+
+
+        sleep(2600);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        wrist.setPosition(WRIST_FOLDED_IN);
+        armMotor.setTargetPosition((int) (0));
+        ((DcMotorEx) armMotor).setVelocity(2100);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        sleep(400);
+        intake.setPower(0);
+        leftActuator.setPower(-ARM_FORWARD_SPEED);
+        rightActuator.setPower(-ARM_FORWARD_SPEED);
+
+
+        sleep(1900);
+
+        leftDrive.setPower(-0.3);
+        rightDrive.setPower(-0.3);
+        sleep(1500);
+        rightDrive.setPower(0.5);
+        leftDrive.setPower(-0.5);
+        sleep(1100);
+
+        // Drive Backwards
+        leftDrive.setPower(-0.5);
+        rightDrive.setPower(-0.5);
+        sleep(4000);
+
+        // END - stop
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
     }
 }
